33fd482eb21f13e36ae9e3b29efea80d856f865c
L3v14th4n <honore.ouedraogo.bc@gmail.com>
2025-07-16 09:49:46 +0000

    fixing auth feature with home dashboard

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="auth/login" options={{ title: 'Login' }} />
       <Stack.Screen name="about" options={{ title: 'About' }} />
     </Stack>
   )
